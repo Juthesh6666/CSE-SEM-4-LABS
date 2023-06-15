abea6663c1399eba25660a869c50936939354cde
Juthesh kumar sunkara <115220478+Juthesh6666@users.noreply.github.com>
2023-06-15 11:23:54 +0530

    Update L6Q1.c

--- 2.ES/LAB6/L6Q1.c
+++ 2.ES/LAB6/L6Q1.c
@@ -9,10 +9,10 @@ int main(void){
 	LPC_GPIO0->FIODIR |= 0x00000FF0;	 	//configure P0.4-P0.11 as output port
 	while(1){
 		LED=0x00000010;					//initial value on LED
-		for (i=1;;i<9;i++){				//on the LEDs serially
+		for (i=1;i<9;i++){				//on the LEDs serially
 			LPC_GPIO0->FIOSET=LED;		//turn on LED at LSB(LED connected to P0.4)
 		
-			for ((j=0;j<10000;j++);		//a random delay
+			for (j=0;j<10000;j++);		//a random delay
 			LED<<=1;					//shift the LED to the left by one unit
 		}								//loop for 8 times
 		LED = 0x00000010;
@@ -22,4 +22,4 @@ int main(void){
 			LED <<= 1;
 		}
 	}
-}		
+}	
